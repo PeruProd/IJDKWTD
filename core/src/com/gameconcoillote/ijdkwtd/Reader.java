abd183395b932d299402770a9d0ae1b7e8d43513
Vincent Soto <vinz.soto@gmail.com>
2015-01-25 12:33:48 +0100

    Create Reader.java

---
+++ core/src/com/gameconcoillote/ijdkwtd/Reader.java
@@ -0,0 +1,16 @@
+package com.gameconcoillote.ijdkwtd;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
+
+/**
+ * Created by Hamor on 25/01/2015.
+ */
+public class Reader {
+    public String read(String langue, String level){
+        FileHandle file = Gdx.files.local("text\\"+langue+"\\"+level+".txt");
+        String txt = file.readString();
+        return txt;
+    }
+
+}
