24cf0c94fd2e3d715722d8002114c1d46da0c912
Bog <berenger.ossete@live.fr>
2015-01-25 14:30:22 +0100

    fix and detect the key

--- core/src/com/gameconcoillote/ijdkwtd/InGameScreen.java
+++ core/src/com/gameconcoillote/ijdkwtd/InGameScreen.java
@@ -45,7 +45,7 @@ public class InGameScreen implements Screen{
 		entities.add(new NoteItem(game,new Texture(Gdx.files.internal("item/note_mur.png")),this.player, 400,214));
 		entities.add(new NoteItem(game,new Texture(Gdx.files.internal("item/note_mur.png")),this.player, 500,214));
 		entities.add(new NoteItem(game,new Texture(Gdx.files.internal("item/note_mur.png")),this.player, 200,214));	
-		entities.add(new KeyItem(game,new Texture(Gdx.files.internal("item/clé_inventaire.png")),this.player, 200,214));	
+		entities.add(new KeyItem(game,new Texture(Gdx.files.internal("item/cle_inventaire.png")),this.player, 200,214));	
 		
 		entities.add(this.notePanel);
 		entities.add(this.notePanel.getCross());
