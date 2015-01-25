cca3fa7e9ca01ceb2ebe1cef4419005ff552ad02
nomce <naum.spaseski@gmail.com>
2015-01-25 13:18:19 +0100

    Added Unix/windows support for Reader

--- core/src/com/gameconcoillote/ijdkwtd/Reader.java
+++ core/src/com/gameconcoillote/ijdkwtd/Reader.java
@@ -3,12 +3,20 @@ package com.gameconcoillote.ijdkwtd;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 
+import java.io.File;
+
 /**
  * Created by Hamor on 25/01/2015.
  */
 public class Reader {
     public String read(String langue, String level){
-        FileHandle file = Gdx.files.local("text\\"+langue+"\\"+level+".txt");
+        FileHandle file;
+        if (File.separatorChar == '/' ){
+            file = Gdx.files.local("text/"+langue+"/"+level+".txt");
+        }else{
+            file = Gdx.files.local("text\\"+langue+"\\"+level+".txt");
+        }
+
         String txt = file.readString();
         return txt;
     }
