c4715d8aaded9da55ed2b2cd22ec1dad912c2759
Bog <berenger.ossete@live.fr>
2015-01-25 09:50:26 +0100

    Added items (cross and arrows) in note panel

--- core/src/com/gameconcoillote/ijdkwtd/NotePanel.java
+++ core/src/com/gameconcoillote/ijdkwtd/NotePanel.java
@@ -25,18 +25,57 @@ public class NotePanel extends Entity
 		
 	}
 	
+	private class ArrowLeftItem extends Item
+	{
+		private NotePanel notePanel;
+		
+		public ArrowLeftItem(NotePanel np) {
+			super(new Texture(Gdx.files.internal("item/fleche_gauche_pleine.png")),0,0);
+			notePanel = np;
+		}
+
+		@Override
+		public void activate() {
+			this.notePanel.setVisible(false);
+		}
+		
+	}
+	
+	private class ArrowRightItem extends Item
+	{
+		private NotePanel notePanel;
+		
+		public ArrowRightItem(NotePanel np) {
+			super(new Texture(Gdx.files.internal("item/fleche_droite_pleine.png")),0,0);
+			notePanel = np;
+		}
+
+		@Override
+		public void activate() {
+			this.notePanel.setVisible(false);
+		}
+		
+	}
+	
 	private InGameScreen igs;
 	private CrossItem cross = new CrossItem(this);
+	private ArrowLeftItem arrowLeft = new ArrowLeftItem(this);
+	private ArrowRightItem arrowRight = new ArrowRightItem(this);
 	
 	public NotePanel(InGameScreen i) {
-		super(new Texture(Gdx.files.internal("item/note_lecture.png")), 0, 0);
+		super(new Texture(Gdx.files.internal("item/note_lecture.png")), 100, 100);
 		this.igs = i;
+		
 		this.igs.addEntity(this.cross);
+		this.igs.addEntity(this.arrowLeft);
+		this.igs.addEntity(this.arrowRight);
 
 	}
 	
 	public void setVisible(boolean b)
 	{
+		this.arrowLeft.visible = b;
+		this.arrowRight.visible = b;
 		this.cross.visible = b;
 		this.visible = b;
 	}
